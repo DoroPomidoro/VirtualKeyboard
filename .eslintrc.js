9e3616a933d4263e481e2da011411f23fadcdccc
DoroPomidoro <pomidorodoro@gmail.com>
2022-05-08 13:22:22 +0300

    refactor: apply eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: ["airbnb-base"],
+    rules: {
+      semi: "off",
+      quotes: ["error", "double"],
+      "linebreak-style": ["error", "windows"],
+    },
+  };
